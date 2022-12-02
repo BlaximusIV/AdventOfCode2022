5bde059e6ef1287815b2f20d6589bd84c221a08d
Neal Darley <darley.neal@gmail.com>
2022-12-01 22:50:14 -0700

    Day 01, pt 2

--- Day01/d01.go
+++ Day01/d01.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -17,21 +18,34 @@ func main() {
 
 	elves := strings.Split(string(content), "\r\n\r\n")
 
-	// Part 1
-	max := 0
+	var elfCalories []int
 	for _, elf := range elves {
-		sum := 0
-		for _, value := range strings.Split(elf, "\r\n") {
-			if i, err := strconv.Atoi(value); err == nil {
-				sum += i
-			}
-		}
+		elfCalories = append(elfCalories, getElfSum(elf))
+	}
 
-		if sum > max {
-			max = sum
+	sort.Slice(elfCalories, func(i, j int) bool {
+		return elfCalories[i] > elfCalories[j]
+	})
+
+	// Part 1
+	fmt.Printf("%d Highest calories\n", elfCalories[0])
+
+	// Part 2
+	topthree := elfCalories[:3]
+	sum := 0
+	for _, val := range topthree {
+		sum += val
+	}
+
+	fmt.Printf("Sum of top 3: %d\n", sum)
+}
+
+func getElfSum(elf string) (sum int) {
+	for _, value := range strings.Split(elf, "\r\n") {
+		if i, err := strconv.Atoi(value); err == nil {
+			sum += i
 		}
 	}
 
-	fmt.Printf("%d Highest calories\n", max)
-
+	return
 }

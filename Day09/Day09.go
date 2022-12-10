b1d6424b11f521c11e4152f2706b1e57937ac2a9
Neal Darley <darley.neal@gmail.com>
2022-12-09 17:26:03 -0700

    Slight refactor

--- Day09/Day09.go
+++ Day09/Day09.go
@@ -31,53 +31,53 @@ func main() {
 	log.Printf("Elapsed Time: %s\n", elapsed)
 }
 
-// This should probably be split into multiple methods and spaced for readability
 func getTailPositionCount(instructionString string, knotCount int) int {
 	knots := []coordinate{}
 	for i := 0; i < knotCount; i++ {
 		knots = append(knots, coordinate{})
 	}
 
-	// tail's position each tick
 	tailVisitedCoordinates := map[coordinate]int{{0, 0}: 1}
-
 	for _, instruction := range strings.Split(instructionString, "\r\n") {
 		pieces := strings.Split(instruction, " ")
 		direction := pieces[0]
 		magnitude, _ := strconv.Atoi(pieces[1])
 
-		switch direction {
-		case "R":
-			for i := 0; i < magnitude; i++ {
-				knots[0] = coordinate{knots[0].X + 1, knots[0].Y}
-				updateKnotPositions(knots, tailVisitedCoordinates)
-			}
-		case "U":
-			for i := 0; i < magnitude; i++ {
-				knots[0] = coordinate{knots[0].X, knots[0].Y + 1}
-				updateKnotPositions(knots, tailVisitedCoordinates)
-			}
-		case "D":
-			for i := 0; i < magnitude; i++ {
-				knots[0] = coordinate{knots[0].X, knots[0].Y - 1}
-				updateKnotPositions(knots, tailVisitedCoordinates)
-			}
-		case "L":
-			for i := 0; i < magnitude; i++ {
-				knots[0] = coordinate{knots[0].X - 1, knots[0].Y}
-				updateKnotPositions(knots, tailVisitedCoordinates)
-			}
+		for i := 0; i < magnitude; i++ {
+			move(&knots[0], direction)
+			updateKnotPositions(knots, tailVisitedCoordinates)
 		}
 	}
 
 	return len(tailVisitedCoordinates)
 }
 
+func move(current *coordinate, direction string) {
+	switch direction {
+	case "R":
+		current.X++
+	case "U":
+		current.Y++
+	case "D":
+		current.Y--
+	case "L":
+		current.X--
+	}
+}
+
+func updateKnotPositions(knots []coordinate, tailVisitedCoordinates map[coordinate]int) {
+	for j := range knots[1:] {
+		knots[j+1] = getNewPosition(knots[j], knots[j+1])
+		if j+2 >= len(knots) {
+			tailVisitedCoordinates[knots[j+1]]++
+		}
+	}
+}
+
 // There has to be a better way to model the behavior. I don't know what it is though
 func getNewPosition(leader coordinate, follower coordinate) (newCoord coordinate) {
 	xDiff := difference(leader.X, follower.X)
 	yDiff := difference(leader.Y, follower.Y)
-
 	newCoord = follower
 
 	if xDiff > 1 {
@@ -119,13 +119,4 @@ func getNewPosition(leader coordinate, follower coordinate) (newCoord coordinate
 
 func difference(a int, b int) int {
 	return int(math.Abs(float64(a - b)))
-}
-
-func updateKnotPositions(knots []coordinate, tailVisitedCoordinates map[coordinate]int) {
-	for j := range knots[1:] {
-		knots[j+1] = getNewPosition(knots[j], knots[j+1])
-		if j+2 >= len(knots) {
-			tailVisitedCoordinates[knots[j+1]]++
-		}
-	}
 }

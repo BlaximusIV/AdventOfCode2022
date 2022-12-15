6646b80316047d6080164e4c9305013dacd22f40
Neal Darley <darley.neal@gmail.com>
2022-12-15 12:11:46 -0700

    Day15, pt2

--- Day15/Day15.go
+++ Day15/Day15.go
@@ -1,3 +1,4 @@
+/* Desperately needs refactoring. Can be made more efficient, as well as clean. */
 package main
 
 import (
@@ -15,7 +16,7 @@ type Point struct {
 	X, Y int
 }
 
-func (p Point) Euclidian(s Point) int {
+func (p Point) EuclidianDistance(s Point) int {
 	return difference(p.X, s.X) + difference(p.Y, s.Y)
 }
 
@@ -24,8 +25,8 @@ func difference(a int, b int) int {
 }
 
 type SensorPair struct {
-	Sensor, Beacon Point
-	Distance       int
+	Sensor, Beacon    Point
+	EuclidianDistance int
 }
 
 type Range struct {
@@ -48,8 +49,12 @@ func main() {
 	populateSensors(&sensorList, string(content))
 
 	// Part1
-	popCount := getDepthMapPopulationCount(sensorList, 2000000)
+	popCount, _ := getDepthMapPopulationCount(sensorList, 2000000)
 	log.Printf("%d items intersecting line\n", popCount)
+
+	// Part2
+	frequency := findMissingBeaconFrequency(sensorList)
+	log.Printf("Missing beacon frequency: %d\n", frequency)
 
 	elapsed := time.Since(startTime)
 	log.Printf("Elapsed Time: %s\n", elapsed)
@@ -72,10 +77,35 @@ func extractPair(rawVals []string) SensorPair {
 		vals = append(vals, val)
 	}
 	sensor, beacon := Point{vals[0], vals[1]}, Point{vals[2], vals[3]}
-	return SensorPair{sensor, beacon, sensor.Euclidian(beacon)}
+	return SensorPair{sensor, beacon, sensor.EuclidianDistance(beacon)}
 }
 
-func getDepthMapPopulationCount(sList []SensorPair, targetDepth int) int {
+func findMissingBeaconFrequency(sensors []SensorPair) int {
+	// For 0..4000000
+	rangeMin := 0
+	rangeMax := 4000000
+	targetPoint := Point{0, 0}
+	for i := rangeMin; i <= rangeMax; i++ {
+		// Get line
+		_, ranges := getDepthMapPopulationCount(sensors, i)
+
+		// Trim it
+		trimmedRanges := getTrimmedRanges(ranges, rangeMin, rangeMax)
+
+		// if there's more than one range, get the hole
+		if len(trimmedRanges) > 1 {
+			targetPoint.X = ranges[0].X2 + 1
+			targetPoint.Y = i
+			break
+		}
+
+	}
+
+	// return calculated frequency
+	return (4000000 * targetPoint.X) + targetPoint.Y
+}
+
+func getDepthMapPopulationCount(sList []SensorPair, targetDepth int) (rangeSum int, ranges []Range) {
 	populatedRanges := []Range{}
 	intersectingItems := []int{}
 
@@ -89,7 +119,7 @@ func getDepthMapPopulationCount(sList []SensorPair, targetDepth int) int {
 		// if the point is even in range
 		// get range
 		convergencePoint := Point{pair.Sensor.X, targetDepth}
-		signedDifference := pair.Distance - pair.Sensor.Euclidian(convergencePoint)
+		signedDifference := pair.EuclidianDistance - pair.Sensor.EuclidianDistance(convergencePoint)
 		if signedDifference >= 0 {
 			r := Range{convergencePoint.X - signedDifference, convergencePoint.X + signedDifference}
 			populatedRanges = append(populatedRanges, r)
@@ -97,15 +127,44 @@ func getDepthMapPopulationCount(sList []SensorPair, targetDepth int) int {
 	}
 
 	// consolidate ranges
-	consolidated := getConsolidatedRanges(populatedRanges)
+	ranges = getConsolidatedRanges(populatedRanges)
 
 	// Count
-	rangeSum := getRangesSum(consolidated)
+	rangeSum = getRangesSum(ranges)
 
 	// Subtract intersecting points
-	rangeSum -= getIntersectingCount(consolidated, intersectingItems)
+	rangeSum -= getIntersectingCount(ranges, intersectingItems)
 
-	return rangeSum
+	return
+}
+
+func getTrimmedRanges(ranges []Range, min int, max int) (trimmedRange []Range) {
+	sort.Slice(ranges, func(i, j int) bool {
+		return ranges[i].X1 < ranges[j].X1
+	})
+
+	minMax := Range{min, max}
+	for _, r := range ranges {
+		if r.X1 >= min && r.X2 <= max {
+			trimmedRange = append(trimmedRange, r)
+			continue
+		}
+		if r.X1 <= min && r.X2 >= max {
+			trimmedRange = append(trimmedRange, Range{min, max})
+			break
+		}
+		if r.X1 <= min && minMax.IsInRange(r.X2) {
+			trimmedRange = append(trimmedRange, Range{min, r.X2})
+			continue
+		}
+		//
+		if minMax.IsInRange(r.X1) && r.X2 >= max {
+			trimmedRange = append(trimmedRange, Range{r.X1, max})
+			break
+		}
+	}
+
+	return
 }
 
 func contains(items []int, item int) bool {
@@ -155,6 +214,7 @@ func getRangesSum(r []Range) (sum int) {
 
 	for _, val := range r {
 		sum += (val.X2 - val.X1)
+		// The range technically contains one more space if it crosses into negative
 		if val.X2 > 0 && val.X1 < 0 {
 			sum++
 		}

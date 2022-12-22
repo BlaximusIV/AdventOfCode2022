1db2c6e056588a3c2fca2a38e3002afbb8cdb8ae
Neal Darley <darley.neal@gmail.com>
2022-12-21 22:07:42 -0700

    Day21, pt2

--- Day21-MonkeyMath/MonkeyMath.go
+++ Day21-MonkeyMath/MonkeyMath.go
@@ -17,18 +17,24 @@ func main() {
 
 	content, _ := os.ReadFile("Input.txt")
 
-	monkeyNumbers := getMonkeyNumbers(string(content))
+	numbers, formulas := getMonkeyInfo(string(content))
 
 	// Part 1
-	fmt.Printf("Root monkey number: %d\n", monkeyNumbers["root"])
+	pOneNumbers := copyNumbers(numbers)
+	populateMonkey("root", pOneNumbers, formulas)
+	fmt.Printf("Root monkey number: %d\n", pOneNumbers["root"])
+
+	// Part 2
+	equalizingNumber := getEqualizingNumber(numbers, formulas)
+	fmt.Printf("Equalizing number: %d\n", equalizingNumber)
 
 	elapsed := time.Since(startTime)
 	fmt.Printf("Elapsed time: %v\n", elapsed)
 }
 
-func getMonkeyNumbers(input string) map[string]int {
-	monkeyNumbers := map[string]int{}
-	monkeyFormulas := map[string]Formula{}
+func getMonkeyInfo(input string) (monkeyNumbers map[string]int, monkeyFormulas map[string]Formula) {
+	monkeyNumbers = map[string]int{}
+	monkeyFormulas = map[string]Formula{}
 	monkeyLines := strings.Split(input, "\r\n")
 	for _, line := range monkeyLines {
 		monkeyInstructions := strings.Split(line, " ")
@@ -42,9 +48,7 @@ func getMonkeyNumbers(input string) map[string]int {
 		}
 	}
 
-	populateMonkey("root", monkeyNumbers, monkeyFormulas)
-
-	return monkeyNumbers
+	return
 }
 
 func populateMonkey(name string, monkeyNumbers map[string]int, formulas map[string]Formula) {
@@ -86,4 +90,68 @@ func getValue(lhs, rhs int, operand string) int {
 	}
 
 	return 0
+}
+
+func getEqualizingNumber(monkeyNumbers map[string]int, monkeyFormulas map[string]Formula) int {
+	guess := 1
+	leftMonkey := 1
+	rightMonkey := 0
+	// right monkey is not dependent on the humn value
+	// Find the point at which lh is less, narrow from there (Currently only functional for real input)
+	for {
+		leftMonkey, rightMonkey = getGuessResult(monkeyNumbers, monkeyFormulas, guess)
+
+		if leftMonkey < rightMonkey {
+			break
+		}
+
+		guess *= 2
+	}
+
+	// Now we know that the correct number is between half the guess number and the current guess
+	low := guess / 2
+	high := guess
+	for leftMonkey != rightMonkey {
+		mid := low + ((high - low) / 2)
+
+		midResult, _ := getGuessResult(monkeyNumbers, monkeyFormulas, mid)
+
+		// We know it's too high
+		if midResult < rightMonkey {
+			high = mid
+		} else if midResult > rightMonkey {
+			low = mid
+		} else {
+			guess = mid
+		}
+		leftMonkey = midResult
+
+		fmt.Printf("Difference: %d\n", leftMonkey-rightMonkey)
+	}
+
+	return guess
+}
+
+func getGuessResult(monkeyNumbers map[string]int, monkeyFormulas map[string]Formula, guess int) (lh int, rh int) {
+	rootFormula := monkeyFormulas["root"]
+	copy := copyNumbers(monkeyNumbers)
+	copy["humn"] = guess
+
+	populateMonkey(rootFormula.lhMonkey, copy, monkeyFormulas)
+	populateMonkey(rootFormula.rhMonkey, copy, monkeyFormulas)
+
+	lh = copy[rootFormula.lhMonkey]
+	rh = copy[rootFormula.rhMonkey]
+
+	return
+}
+
+// There should be a better way to do a deep copy in go. I don't know what it is yet. Need to move on.
+func copyNumbers(monkeyNumbers map[string]int) (copy map[string]int) {
+	copy = map[string]int{}
+
+	for key, val := range monkeyNumbers {
+		copy[key] = val
+	}
+	return
 }

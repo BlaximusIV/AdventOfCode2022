18fa8babb37cb9ee3a9d6a7c5b50349f37356cdc
Neal Darley <darley.neal@gmail.com>
2022-12-22 13:41:13 -0700

    Day18, pt2

--- Day18-BoilingBoulders/BoilingBoulders.go
+++ Day18-BoilingBoulders/BoilingBoulders.go
@@ -21,14 +21,12 @@ func (c Cube) GetNeighbors() []Cube {
 	return []Cube{{c.x + 1, c.y, c.z}, {c.x - 1, c.y, c.z}, {c.x, c.y + 1, c.z}, {c.x, c.y - 1, c.z}, {c.x, c.y, c.z + 1}, {c.x, c.y, c.z - 1}}
 }
 
-func (c Cube) CountExposed(drop Droplet, excludeBubbles bool) int {
+func (c Cube) CountExposed(drop Droplet) int {
 	borderedSides := 0
 	borders := c.GetNeighbors()
 
 	for i := range borders {
 		if drop.Cubes[borders[i]] {
-			borderedSides++
-		} else if excludeBubbles && borders[i].IsBubbleCoord(drop) {
 			borderedSides++
 		}
 	}
@@ -36,93 +34,22 @@ func (c Cube) CountExposed(drop Droplet, excludeBubbles bool) int {
 	return 6 - borderedSides
 }
 
-func (c Cube) IsBubbleCoord(drop Droplet) bool {
-	// If there is a block in all six directions
-	// This assumes no hooks (J) or negative coordinates, and seems to be correct
-	// x, false if goes to max or min
-	x := c.x + 1
-	for x < drop.MaxX {
-		if drop.Cubes[Cube{x, c.y, c.z}] {
-			break
-		}
-		x++
-	}
-	if x >= drop.MaxX {
-		return false
-	}
-	nx := c.x - 1
-	for nx > 0 {
-		if drop.Cubes[Cube{nx, c.y, c.z}] {
-			break
-		}
-		nx--
-	}
-	if nx <= 0 {
-		return false
-	}
-	y := c.y + 1
-	for y < drop.MaxY {
-		if drop.Cubes[Cube{c.x, y, c.z}] {
-			break
-		}
-		y++
-	}
-	if y >= drop.MaxY {
-		return false
-	}
-	ny := c.y - 1
-	for y > 0 {
-		if drop.Cubes[Cube{c.x, ny, c.z}] {
-			break
-		}
-		y--
-	}
-	if ny <= 0 {
-		return false
-	}
-	z := c.z + 1
-	for z <= drop.MaxZ {
-		if drop.Cubes[Cube{c.x, c.y, z}] {
-			break
-		}
-		z++
-	}
-	if z >= drop.MaxZ {
-		return false
-	}
-	nz := c.z - 1
-	for nz >= 0 {
-		if drop.Cubes[Cube{c.x, c.y, nz}] {
-			break
-		}
-		nz--
-	}
-	if nz <= 0 {
-		return false
-	}
-
-	return true
-}
-
 func main() {
 	startTime := time.Now()
 
-	content, _ := os.ReadFile("TestInput.txt")
+	content, _ := os.ReadFile("Input.txt")
 
 	drop := getDrop(string(content))
 
 	// Part 1
 	sum := 0
 	for c := range drop.Cubes {
-		sum += c.CountExposed(drop, false)
+		sum += c.CountExposed(drop)
 	}
 	log.Printf("Surface Area: %d", sum)
 
 	// Part 2
-	sum = 0
-	for c := range drop.Cubes {
-		sum += c.CountExposed(drop, true)
-	}
+	sum = getFloodObstructionCount(drop)
 
 	log.Printf("Surface Area: %d", sum)
 
@@ -150,4 +77,66 @@ func getDrop(input string) Droplet {
 		cubes[Cube{x, y, z}] = true
 	}
 	return Droplet{cubes, maxx, maxy, maxz}
+}
+
+// Basically BFS
+func getFloodObstructionCount(drop Droplet) int {
+	visited := map[Cube]bool{}
+	frontier := []Cube{{-1, -1, -1}}
+	obstructionCount := 0
+	for len(frontier) > 0 {
+		cube := frontier[0]
+		visited[cube] = true
+		frontier = frontier[1:]
+
+		neighbors := cube.GetNeighbors()
+
+		inBoundNeighbors := []Cube{}
+		for _, val := range neighbors {
+			if val.x < -1 || val.x > drop.MaxX+1 {
+				continue
+			} else if val.y < -1 || val.y > drop.MaxY+1 {
+				continue
+			} else if val.z < -1 || val.z > drop.MaxZ+1 {
+				continue
+			}
+
+			inBoundNeighbors = append(inBoundNeighbors, val)
+		}
+
+		nonQueuedNeighbors := []Cube{}
+		for _, val := range inBoundNeighbors {
+			contains := false
+			for _, frontierVal := range frontier {
+				if val == frontierVal {
+					contains = true
+					break
+				}
+			}
+			if !contains {
+				nonQueuedNeighbors = append(nonQueuedNeighbors, val)
+			}
+		}
+
+		nonBlockedNeighbors := []Cube{}
+		for _, val := range nonQueuedNeighbors {
+			if drop.Cubes[val] {
+				// entry from this direction is unavailable. Counts as a single face of a single lava drop block
+				obstructionCount++
+			} else {
+				nonBlockedNeighbors = append(nonBlockedNeighbors, val)
+			}
+		}
+
+		nonVisitedNeighbors := []Cube{}
+		for _, val := range nonBlockedNeighbors {
+			if !visited[val] {
+				nonVisitedNeighbors = append(nonVisitedNeighbors, val)
+			}
+		}
+
+		frontier = append(frontier, nonVisitedNeighbors...)
+	}
+
+	return obstructionCount
 }
